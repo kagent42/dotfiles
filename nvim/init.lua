c0d5499ea795c517018b3eb3ca8f49b3a0d1e103
Katherine Agent <kagent42@pm.me>
2024-10-03 12:28:44 -0700

    darker bg for catpuccin

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,5 +2,12 @@
 require("config.lazy")
 
 -- Theme
+require("catppuccin").setup({
+  color_overrides = {
+    all = {
+      base = "#1F1F1F",
+    },
+  },
+})
 vim.cmd.colorscheme("catppuccin-mocha")
 require("config.keymaps")
--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -16,6 +16,7 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
   },
+  -- Other
   {
     "christoomey/vim-tmux-navigator",
     cmd = {
